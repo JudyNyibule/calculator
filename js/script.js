aebac366c283e9a7d8653f6d67a7aef0b05e1b07
Judy Nyibule <judynyibule@gmail.com>
2018-11-26 10:17:29 +0300

    First commit for Push

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add=function(a,b){
+return a+b;
+}
+
+var myNumber1=parseInt(prompt('Enter a number 1:'));
+var myNumber2=parseInt(prompt('Enter a number 2:'));
+alert(add(myNumber1, myNumber2));
